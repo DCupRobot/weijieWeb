5b1d749124b7af47f1d6371e189de5f5b3fbdd06
DCupRobot <dwjandzzs@gmail.com>
2020-10-05 11:31:17 +0800

    更新了手机版背景

--- weijieWeb/frontend/src/components/main_page.vue
+++ weijieWeb/frontend/src/components/main_page.vue
@@ -1,24 +1,58 @@
 <template>
-  <div class="background" :style="backgroundImageRandom">
-  </div>
+<div class="background" :style="backgroundImageRandom"></div>
 </template>
 
 <script>
+const bgcUrlPc = "url(" +
+  require("../assets/img/background_PC/" + Math.floor(Math.random() * 30 + 1) + ".jpg") +
+  ")";
+const bgcUrlMb = "url(" +
+  require("../assets/img/background_MB/" + Math.floor(Math.random() * 30 + 1) + ".jpg") +
+  ")";
 export default {
-  
-  name: 'main_page',
+  name: "main_page",
   data: () => ({
     backgroundImageRandom: {
-      backgroundImage: 'url(' + require('../assets/img/background/' + Math.floor(Math.random() * 30 + 1) + '.jpg') + ')',
-    },
+      backgroundImage: bgcUrlPc
+    }
   }),
-  methods: {
-    toggleModalState() {
-      this.modalOpen = !this.modalOpen;
-    },
+  created() {
+    let clientEq = this.checkClientEq();
+    if (clientEq.isPc == false) {
+      this.backgroundImageRandom = {
+        backgroundImage: bgcUrlMb
+      }
+    }
   },
+  methods: {
+    checkClientEq() {
+      let ua = navigator.userAgent,
+        isWindowsPhone = /(?:Windows Phone)/.test(ua),
+        isSymbian = /(?:SymbianOS)/.test(ua) || isWindowsPhone,
+        isAndroid = /(?:Android)/.test(ua),
+        isFireFox = /(?:Firefox)/.test(ua),
+        isChrome = /(?:Chrome|CriOS)/.test(ua),
+        isTablet =
+        /(?:iPad|PlayBook)/.test(ua) ||
+        (isAndroid && !/(?:Mobile)/.test(ua)) ||
+        (isFireFox && /(?:Tablet)/.test(ua)),
+        isPhone = /(?:iPhone)/.test(ua) && !isTablet,
+        isPc = !isPhone && !isAndroid && !isSymbian;
+      console.log({
+        isTablet: isTablet,
+        isPhone: isPhone,
+        isAndroid: isAndroid,
+        isPc: isPc
+      });
+      return {
+        isTablet: isTablet,
+        isPhone: isPhone,
+        isAndroid: isAndroid,
+        isPc: isPc
+      };
+    }
+  }
 };
 </script>
-<style scoped>
 
-</style>
+<style scoped></style>
